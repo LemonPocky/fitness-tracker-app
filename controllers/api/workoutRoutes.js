ac707b42d5806fe6c925a9da23b6e361d3c22fac
LemonPocky <someone-70@live.com>
2021-08-10 20:44:29 -0700

    Added routes to create a new workout and add an exercise

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -2,13 +2,45 @@ const router = require('express').Router();
 const Workout = require('../../models/workout');
 
 // Returns all workouts
-router.get('/', (req, res) => {});
+router.get('/', async (req, res) => {
+  try {
+    const docs = await Workout.aggregate().addFields({
+      totalDuration: { $sum: '$exercises.distance' },
+    });
+    res.json(docs);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error);
+  }
+});
 
 // Create a new workout
-router.post('/', (req, res) => {});
+router.post('/', async (req, res) => {
+  try {
+    const data = await Workout.create(req.body);
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error);
+  }
+});
 
 // Add an exercise to this workout
-router.put('/:id', (req, res) => {});
+router.put('/:id', async (req, res) => {
+  try {
+    const data = await Workout.findOneAndUpdate(
+      { _id: req.params.id },
+      { $push: { exercises: req.body } },
+      { new: true }
+    );
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error);
+  }
+});
 
 // Returns last 7 workouts
 router.get('/range', (req, res) => {});
+
+module.exports = router;
--- models/workout.js
+++ models/workout.js
@@ -2,24 +2,28 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const WorkoutSchema = new Schema({
-  day: {
-    type: Date,
-  },
-
-  // Exercises is an array of exercise objects.
-  exercises: [
-    {
-      type: String,
-      name: String,
-      distance: Number,
-      duration: Number,
-      weight: Number,
-      reps: Number,
-      sets: Number,
+const WorkoutSchema = new Schema(
+  {
+    day: {
+      $type: Date,
+      default: Date.now,
     },
-  ],
-});
+
+    // Exercises is an array of exercise objects.
+    exercises: [
+      {
+        type: String,
+        name: String,
+        distance: Number,
+        duration: Number,
+        weight: Number,
+        reps: Number,
+        sets: Number,
+      },
+    ],
+  },
+  { typeKey: '$type' }
+);
 
 const Workout = mongoose.model('Workout', WorkoutSchema);
 
