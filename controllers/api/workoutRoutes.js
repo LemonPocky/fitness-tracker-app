432af3274724898b6cd3f4765d0d1b78c224192c
LemonPocky <someone-70@live.com>
2021-08-10 21:14:17 -0700

    Fetch last 7 records in range API (without aggregation)

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -41,6 +41,17 @@ router.put('/:id', async (req, res) => {
 });
 
 // Returns last 7 workouts
-router.get('/range', (req, res) => {});
+router.get('/range', async (req, res) => {
+  try {
+    // Get 7 latest entries by sorting from highest to lowest
+    const data = await Workout.find().sort({ _id: -1 }).limit(7);
+    // Sort array from lowest to highest id
+    data.sort((a, b) => (a._id < b._id ? -1 : 1));
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error);
+  }
+});
 
 module.exports = router;
